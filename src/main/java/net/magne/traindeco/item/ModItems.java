228c0d69634f23251dd0b758b6e605e4cd8f70f6
MagnemusL <magnelindholm@outlook.dk>
2023-06-02 19:01:58 +0200

    Some New Secret Items And Some Conflict Resolving.

--- src/main/java/net/magne/traindeco/item/ModItems.java
+++ src/main/java/net/magne/traindeco/item/ModItems.java
@@ -1,14 +1,25 @@
 package net.magne.traindeco.item;
 
 import net.magne.traindeco.TrainDeco;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TrainDeco.MOD_ID);
+
+    public static final RegistryObject<Item> HAMBURGER = ITEMS.register("hamburger",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> COLA = ITEMS.register("cola",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> SIGN_ITEM = ITEMS.register("sign_item",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
 
     public static void register(IEventBus eventBus) {
